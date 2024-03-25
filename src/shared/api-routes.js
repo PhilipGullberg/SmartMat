efcf3a42db1d23223a32d2f309250da953f959b8
Philip Gullberg <philip_gullberg@hotmail.com>
2024-03-25 22:27:40 +0100

    Update api-routes.js

--- src/shared/api-routes.js
+++ src/shared/api-routes.js
@@ -1,4 +1,4 @@
-const DEFAULT_ROUTE = "https://jellyfish-app-42zln.ondigitalocean.app/api";
+const DEFAULT_ROUTE = "https://jellyfish-app-42zln.ondigitalocean.app/api/";
 
 export const apiRoutes = {
   users: DEFAULT_ROUTE + "users/",
